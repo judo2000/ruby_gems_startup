8abc5243cb3277e87807e82f22323530369ff055
Stephen Moore <judo2000@comcast.net>
2021-05-06 15:33:31 -0600

    add confirmable to devise registration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_05_224559) do
+ActiveRecord::Schema.define(version: 2021_05_06_210244) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -73,7 +73,7 @@ ActiveRecord::Schema.define(version: 2021_05_05_224559) do
   end
 
 # Could not dump table "users" because of following StandardError
-#   Unknown type 'inet' for column 'current_sign_in_ip'
+#   Unknown type '' for column 'current_sign_in_ip'
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
